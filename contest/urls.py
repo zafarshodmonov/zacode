332b339ffe9e4dd8126f97417e1d3eda60983f67
zafarshodmonov1996 <zafarshodmonov1996@gmail.com>
2025-04-30 22:40:17 +0500

    z

--- contest/urls.py
+++ contest/urls.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    # Matches the root URL ("/") and calls the 'home' view
+    path('', views.base, name='base'),
     path('admin/', admin.site.urls),
+    path('problems/', views.problem_list, name='problem_list'),
     path('contests/', views.contest_list, name='contest_list'),
+    path('topics/', views.topic_list, name='topic_list'),
+    path('topics/<int:pk>/', views.topic_detail, name='topic_detail'),
     path('contests/<int:contest_id>/', views.contest_detail, name='contest_detail'),
     path('contests/<int:contest_id>/<str:problem_index>/', views.problem_detail, name='problem_detail'),
 ]
--- contest/views.py
+++ contest/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from .models import Problem, Contest, Topic
 
 def problem_detail(request, contest_id, problem_index):
@@ -19,6 +20,50 @@ def contest_list(request):
     return render(request, 'contests/contest_list.html', {
         'contests': contests,
     })
+
+def base(request):
+    return render(request, 'base.html')
+
+def problem_list(request):
+    query = request.GET.get('q', '')
+    topic_id = request.GET.get('topic', '')
+
+    problems = Problem.objects.all()
+
+    if query:
+        problems = problems.filter(
+            Q(title__icontains=query) | Q(statement__icontains=query)
+        )
+
+    if topic_id:
+        problems = problems.filter(topics__id=topic_id)
+
+    topics = Topic.objects.all()
+
+    context = {
+        'problems': problems.order_by('contest__start_time', 'index'),
+        'query': query,
+        'topics': topics,
+        'selected_topic_id': topic_id,
+    }
+    return render(request, 'problems/problem_list.html', context)
+
+
+def topic_list(request):
+    topics = Topic.objects.all()
+    context = {
+        'topics': topics,
+    }
+    return render(request, 'topics/topic_list.html', context)
+
+
+def topic_detail(request, pk):
+    topic = get_object_or_404(Topic, id=pk)
+    problems = Problem.objects.filter(topics=topic)
+    return render(request, 'topics/topic_detail.html', {'topic': topic, 'problems': problems})
+
+
+
 
 
 
